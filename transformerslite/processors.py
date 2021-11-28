7197b0dde41a77a30b4db8ecb93eed7c2712136b
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2021-11-28 16:21:57 +0530

    ups

--- transformerslite/processors.py
+++ transformerslite/processors.py
@@ -12,10 +12,15 @@ class ClassificationProcessor:
   def process(self, examples):
 
     self.examples = examples
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
-    model_inputs = self.tokenizer(examples['sentence'], max_length=self.max_input_length, truncation=True, padding=True)
 
-    return tokenizer, model_inputs
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
+    model_inputs = tokenizer(examples['sentence'], max_length=self.max_input_length, truncation=True, padding=True)
+
+    return model_inputs
+  
+  def tokenizerfunction(self):
+
+    return AutoTokenizer.from_pretrained("bert-base-uncased", use_fast=True)
 
 
 class T5Seq2SeqProcessor:
@@ -35,17 +40,20 @@ class T5Seq2SeqProcessor:
 
     tokenizer = AutoTokenizer.from_pretrained("t5-small", use_fast=True)
     inputs = [self.prefix + doc for doc in self.examples["source"]]
-    model_inputs = self.tokenizer(inputs, max_length=self.max_input_length, truncation=True, padding=True)
+    model_inputs = tokenizer(inputs, max_length=self.max_input_length, truncation=True, padding=True)
 
-    # Setup the tokenizer for targets
-    with self.tokenizer.as_target_tokenizer():
-        labels = self.tokenizer(self.examples["target"], max_length=self.max_target_length, truncation=True, padding=True)
+    with tokenizer.as_target_tokenizer():
+        labels = tokenizer(self.examples["target"], max_length=self.max_target_length, truncation=True, padding=True)
 
     model_inputs["labels"] = labels["input_ids"]
     
-    return tokenizer, model_inputs 
+    return model_inputs 
   
+  def tokenizer_function(self):
+    
+    return AutoTokenizer.from_pretrained("t5-small", use_fast=True)
   
+
 class LanguageModelProcessor:
   """
   Processor for T5 models
@@ -59,6 +67,10 @@ class LanguageModelProcessor:
 
     self.examples = examples
     tokenizer = AutoTokenizer.from_pretrained("gpt-2", use_fast=True)
-    model_inputs = self.tokenizer(examples['sentence'], max_length=self.max_input_length, truncation=True, padding=True)
+    model_inputs = tokenizer(examples['sentence'], max_length=self.max_input_length, truncation=True, padding=True)
 
-    return tokenizer, model_inputs
+    return model_inputs
+
+  def tokenizer_function(self):
+
+    return AutoTokenizer.from_pretrained("gpt-2", use_fast=True)
