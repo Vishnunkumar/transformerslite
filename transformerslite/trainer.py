56ba7038132c8ccb29bd4bd7ae70a380172c67eb
Vishnu Nandakumar <38393302+Vishnunkumar@users.noreply.github.com>
2021-11-29 09:39:30 +0530

    Update trainer.py

--- transformerslite/trainer.py
+++ transformerslite/trainer.py
@@ -1,9 +1,19 @@
 from transformers import AutoModelForSeq2SeqLM, AutoModelForSequenceClassification, AutoModelForCausalLM, AutoModelForMaskedLM, TrainingArguments, Trainer, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from processor import *
 
 class SeqClassifier:
 
-    def __init__(self, dataset, model, tokenizer):
+    def __init__(self, dataset, epochs, batch_size, learning_rate):
 
         self.dataset = dataset
-        self.model = model
-        self.tokenizer = tokenizer
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        
+    def fit(self, max_input_length):
+        
+        self.max_input_length = max_input_length
+        preprocessor = Class
+        
+        
+        
